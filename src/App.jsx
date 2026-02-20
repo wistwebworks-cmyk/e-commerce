b17109ab245333ce0398c999a1f7338c287fe3c0
Wist Webworks <wistwebworks@gmail.com>
2026-02-20 23:53:07 +0600

    router dom and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,39 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Layout
-import Layout from "./components/layout/Layout";
-
-// Pages
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Products from "./pages/Shop";
-import Cart from "./pages/Cart";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import Confirm from "./pages/Confirm"; 
-import Admin from "./pages/Admin";
-import Shipping from "./pages/Shipping";
-import Refund from "./pages/Refund";
-import Thanks from "./pages/Thanks";
-import Details from "./pages/Details";
+import './App.css';
+import Navbar from './components/navbar/Navbar.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Shop from './pages/Shop.jsx';
+import Cart from './pages/Cart.jsx';
+import Details from './pages/Details.jsx';
+import Confirm from './pages/Confirm.jsx';
+import Thanks from './pages/Thanks.jsx';
+import Refund from './pages/Refund.jsx';
+import Login from './pages/Login.jsx';
+import Shipping from './pages/Shipping.jsx';
+import Admin from './pages/Admin.jsx';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-    
-        <Route element={<Layout />}>
-          {/* Public Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/product/:id" element={<Details />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Confirm />} />
-          <Route path="/confirm" element={<Confirm />} />
-          <Route path="/shipping" element={<Shipping />} />
-          <Route path="/refund" element={<Refund />} />
-          <Route path="/thanks" element={<Thanks />} />
-        </Route>
-    
-      
-      
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </Router>
-  );
-}
-
+    return (
+        <BrowserRouter>
+            <div> 
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/details" element={<Details />} />
+                    <Route path="/confirm" element={<Confirm />} />
+                    <Route path="/thanks" element={<Thanks />} />
+                    <Route path="/refund" element={<Refund />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/shipping" element={<Shipping />} />
+                    <Route path="/admin" element={<Admin />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+}       
 export default App;
